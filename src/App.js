0e0e28f84e5653e1e2221085ef8196a2a7e57127
vvk <vikram.karamsetty@gmail.com>
2022-09-03 21:30:49 -0400

    Added user input to change letters at random.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import RandomLetter from './components/RandomLetter/RandomLetter';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [num, setNum] = useState(randomNumberInRange(0,26)); // starting off num as a random number
+
+  function randomNumberInRange(min, max) {
+    // 👇️ get number between min (inclusive) and max (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+
+  useEffect(() => { // taking input and then removing it so it doesn't repeat 
+    window.addEventListener('keydown', detectKeyDown);
+    return () => {
+      window.removeEventListener('keydown', detectKeyDown);
+    };
+  }, []);
+
+  const detectKeyDown = (e) => { // what to do after detecting input
+    setNum(randomNumberInRange(0,26))
+    console.log(e.key)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RandomLetter nums={num} />
     </div>
   );
 }
---
+++ src/components/RandomLetter/RandomLetter.js
@@ -0,0 +1,11 @@
+import './RandomLetter.css';
+const RandomLetter = props => {
+    let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    return (
+        <div className="bigLetter">
+            <h1>{alphabet[props.nums]}</h1>
+        </div>
+    );
+};
+
+export default RandomLetter;
