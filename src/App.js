aa3efa23a542778683eef59c17bbc28185804774
vvk <vikram.karamsetty@gmail.com>
2022-10-02 22:31:38 -0400

    Set up React Router

--- src/App.js
+++ src/App.js
@@ -1,132 +1,16 @@
-import { Component, useEffect, Text } from 'react'
-import './App.css';
-import RandomLetter from './components/RandomLetter/RandomLetter';
-import Right from './components/RightOrWrong/Right';
-import Wrong from './components/RightOrWrong/Wrong';
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import FirstStep from './Pages/FirstStep';
 
-const randomNumberInRange = (min, max) => {
-    // 👇️ get number between min (inclusive) and max (inclusive)
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function App() {
+  return (
+    <Router>
+        <Routes>
+            <Route path="/"  />
+            <Route path="/First-Step" element={<FirstStep />} />
+        </Routes>
+    </Router>
+  )
 }
 
-
-
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            letter: -1,
-            pNum: -1,
-            tNum: 0,
-            rights: 0,
-            wrongs: 0,
-            rightOrWrong: false,
-            percentage : 0
-        }
-        this.keyDown = this.keyDown.bind(this);
-    }
-
-
-    componentDidMount() {
-        this.setState({ letter: 'A', pNum: -1, tNum: randomNumberInRange(0, 26), rights: 0, wrongs: 0, rightOrWrong:false, percentage:0 })
-    }
-
-    keyDown(e) {
-
-        var newState = {
-            letter: e.key,
-            pNum: this.state.tNum,
-            tNum: randomNumberInRange(0, 26),
-            rights: this.state.rights,
-            wrongs: this.state.wrongs,
-            rightOrWrong: false,
-            percentage: this.state.rights/(this.state.rights+this.state.wrongs)
-        }
-
-
-        let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'SPACEBAR'];
-
-        if(newState.rights+newState.wrongs === 30) {
-            console.log("over 30")
-            // sends to another
-        }
-
-        document.body.style = 'background: white';
-        if (newState.pNum === -1) { // making first time you load empty
-
-        } else if (newState.letter === " " && newState.pNum === 26) {
-            console.log("right, spacebar clicked")
-            newState.rights++;
-            // return <Right />
-            newState.rightOrWrong=true;
-        } else if (newState.letter === alphabet[newState.pNum]) {
-            console.log("right");
-            // this.updateProgress(1,0);
-            newState.rights++;
-            // return <Right />
-
-            newState.rightOrWrong = true;
-        } else {
-            console.log("wrong");
-            // this.updateProgress(0,1);
-            newState.wrongs++;
-            // return <Wrong />
-            newState.rightOrWrong = false;
-        }
-
-        this.setState(newState);
-    }
-
-    render() {
-        const { letter, pNum, tNum, rights, wrongs, rightOrWrong, percentage } = this.state;
-
-        let rightorwrong = true;
-        if(rightOrWrong) {
-            rightorwrong = <Right />
-        } else {
-            rightorwrong = <Wrong />
-        }
-
-        // PERCENTAGE IS ONE BEHIND, SIMILAR TO PREVIOUS ISSUE
-        let perc = (percentage*100).toFixed(0) // cutting the percentage up till the last 2 decimal points
-        return (
-            <div>
-                <RandomLetter nums={this.state.tNum} />
-                {rightorwrong}
-                
-                {/* letter: {letter}
-                <br />
-                tNum: {tNum}
-                <br />
-                pNum: {pNum}
-                <br /> */}
-                <KeyPressed keyDown={this.keyDown} />
-                <br />
-                <div className='info'>
-                    Correct: {rights}         Incorrect: {wrongs}
-                    <br />
-                    Percentage Correct: {perc}%
-                    
-                </div>
-                
-            </div>
-        );
-    }
-}
-
-const KeyPressed = props => {
-
-    useEffect(() => { // taking input and then removing it so it doesn't repeat 
-        window.addEventListener('keydown', detectKeyDown);
-        return () => {
-            window.removeEventListener('keydown', detectKeyDown);
-        };
-    }, []);
-
-    var detectKeyDown = (e) => { // what to do after detecting input
-        props.keyDown(e);
-    }
-
-    return <div></div>
-}
+export default App;
