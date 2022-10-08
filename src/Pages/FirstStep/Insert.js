da6759d046cbd834685e9f74619d5b6afa404f7f
vvk <vikram.karamsetty@gmail.com>
2022-10-07 23:00:43 -0400

    Started work on hand and circles for FirstStep

--- src/Pages/FirstStep/Insert.js
+++ src/Pages/FirstStep/Insert.js
@@ -3,6 +3,10 @@ import Right from '../../components/RightOrWrong/Right';
 import Wrong from '../../components/RightOrWrong/Wrong';
 import { useEffect } from 'react'
 import RandomLetter from '../../components/RandomLetter/RandomLetter';
+import './Insert.css'
+import rightHand from '../../resources/RightHand.png';
+import leftHand from '../../resources/LeftHand.png';
+
 
 function Insert(props) {
 
@@ -14,38 +18,49 @@ function Insert(props) {
                 window.removeEventListener('keydown', detectKeyDown);
             };
         }, []);
-    
+
         var detectKeyDown = (e) => { // what to do after detecting input
             props.keyDown(e);
         }
-    
+
         return <div></div>
     }
 
     let rightorwrong = true;
-        if (props.rightOrWrong) {
-            rightorwrong = <Right />
-        } else {
-            rightorwrong = <Wrong />
-        }
-    
-  return (
-    
-    <div>
-        <RandomLetter nums={props.tNum} />
-                {rightorwrong}
+    if (props.rightOrWrong) {
+        rightorwrong = <Right />
+    } else {
+        rightorwrong = <Wrong />
+    }
 
-                <KeyPressed keyDown={props.keyDown} />
+    return (
+
+        <div>
+            <RandomLetter nums={props.tNum} />
+            {rightorwrong}
+
+            <KeyPressed keyDown={props.keyDown} />
+            <br />
+            <div className='info'>
+                Correct: {props.rights}         Incorrect: {props.incorrect}
                 <br />
-                <div className='info'>
-                    Correct: {props.rights}         Incorrect: {props.incorrect}
-                    <br />
-                    Percentage Correct: {props.perc}%
+                Percentage Correct: {props.perc}%
 
+            </div>
+            <div className='container'>
+                <div className='circle'></div>
+            </div>
+            <div className='handsContainer'>
+                <img src={leftHand} className="lefthand"></img>
+                {/* <div id="space"></div> */}
+                <div className='rightHandContainer'>
+                    <img src={rightHand} className="righthand"></img>
                 </div>
 
-    </div>
-  )
+            </div>
+
+        </div>
+    )
 }
 
 export default Insert
