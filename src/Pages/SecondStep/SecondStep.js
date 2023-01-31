0647cb9ad025ca59e3fadd592a5389fe2a6aa836
vvk <vikram.karamsetty@gmail.com>
2023-01-30 20:03:15 -0500

    Tinkered with changing style states

--- src/Pages/SecondStep/SecondStep.js
+++ src/Pages/SecondStep/SecondStep.js
@@ -9,40 +9,51 @@ function SecondStep() {
   //plan: make the word into a char array then make a subsequent style obj array of the same length and then when you use the map() function display every word you give the element a corresponding style in the style array
   // make a position integer to hold the index that the person is on for them to type at
 
-  // const KeyPressed = props => {
-
-  //   useEffect(() => { // taking input and then removing it so it doesn't repeat 
-  //     window.addEventListener('keydown', detectKeyDown);
-  //     return () => {
-  //       window.removeEventListener('keydown', detectKeyDown);
-  //     };
-  //     // eslint-disable-next-line
-  //   }, []);
-
-  //   var detectKeyDown = (e) => { // what to do after detecting input
-
-  //   }
-
-  //   return <div></div>
-  // }
-
-
-
   /* INPUT FROM FILE */
   const [text, setText] = useState("");
 
   useEffect(() => {
     Axios(txt).then(res => setText(res.data)); // This will have your text inside data attribute
   }, [])
-  console.log(text);
 
   const words = text.split('\r\n');
-  console.log(words);
   /* =========== */
+  
+  // let [pointer, setPointer] = useState(0);
+  // let[styles, setStyles] = useState();
+  let styles = []
+  const KeyPressed = props => {
+
+    useEffect(() => { // taking input and then removing it so it doesn't repeat 
+      window.addEventListener('keydown', detectKeyDown);
+      return () => {
+        window.removeEventListener('keydown', detectKeyDown);
+      };
+      // eslint-disable-next-line
+    }, []);
+
+    var detectKeyDown = (e) => { // what to do after detecting input
+      changeWord();
+      console.log(styles[0])
+      // console.log(e);
+      // setPointer(pointer+1);
+    }
+
+    return <div></div>
+  }
+
+// USE A STATE
+  
+  function changeWord() {
+    // setStyles(styles[0] = "typed")
+    styles[0] = "typed";
+    console.log(styles[0])
+  }
+  
 
   let wordInd = Math.floor(Math.random() * words.length); // 0-words.length-1
 
-  let styles = [];
+  
   for (let i = 0; i < words[wordInd].length; i++) {
     styles.push("unTyped");
   }
@@ -54,7 +65,6 @@ function SecondStep() {
 
 
   const fullWord = charString.map((letter, ind) => {
-    console.log(ind);
     return (<h1 className={styles[ind]} key={letter}>{letter}</h1>)
   });
 
@@ -69,6 +79,7 @@ function SecondStep() {
     //   exit={{ opacity: 0, x: 100 }}
     // >
     <div className='container'>
+      <KeyPressed />
       {fullWord}
     </div>
 
