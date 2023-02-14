9f31b69f9a8e5d705676d1d9261990a377426cea
vvk <vikram.karamsetty@gmail.com>
2023-02-13 22:24:18 -0500

    Added hands from Insert.js into SecondStep and styled

--- src/Pages/SecondStep/SecondStep.js
+++ src/Pages/SecondStep/SecondStep.js
@@ -1,8 +1,10 @@
 // import { motion } from 'framer-motion';
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import './SecondStep.css';
 import txt from './Words.txt';
 import Axios from "axios"; // Import Axios or use Fetch.
+import rightHand from '../../resources/RightHand.png';
+import leftHand from '../../resources/LeftHand.png';
 
 function SecondStep() {
 
@@ -18,9 +20,9 @@ function SecondStep() {
 
   const words = text.split('\r\n');
   /* =========== */
-  
+
   let [pointer, setPointer] = useState(0);
-  let[styles, setStyles] = useState([]);
+  let [styles, setStyles] = useState([]);
 
   const KeyPressed = props => {
 
@@ -33,8 +35,8 @@ function SecondStep() {
     }, []);
 
     var detectKeyDown = (e) => { // what to do after detecting input
-      changeWord(pointer,e);
-      setPointer(pointer+1);
+      changeWord(pointer, e);
+      setPointer(pointer + 1);
       console.log(e)
       // console.log(e);
     }
@@ -44,7 +46,7 @@ function SecondStep() {
 
 
   const badKeys = ['Control', 'Alt', 'Enter', 'Shift'];
-// USE A STATE
+  // USE A STATE
   function changeWord(index, keyInfo) {
     const updateStyle = styles.map((value, ind) => {
       // SUPPOSED TO IGNORE BAD KEYS
@@ -54,10 +56,10 @@ function SecondStep() {
       //   return "unTyped";
       // } 
       // ===============
-       if (ind === index && keyInfo.key == words[wordInd].charAt(pointer)) { // seeing if its the right letter
+      if (ind === index && keyInfo.key == words[wordInd].charAt(pointer)) { // seeing if its the right letter
         // Increment the clicked counter
         return "correct";
-      } else if(ind === index ){ // seeing if its the right letter
+      } else if (ind === index) { // seeing if its the right letter
         // The rest haven't changed
         return "wrong";
       } else { // seeing if its a random letter
@@ -78,14 +80,14 @@ function SecondStep() {
       setPointer(0);
       setStyles(newStyle);
     }, 500);
-    
+
   }
-  
+
 
   // let wordInd = Math.floor(Math.random() * words.length); // 0-words.length-1
-  const[wordInd, setWordInd] = useState(Math.floor(Math.random() * words.length)); // starts with the first one 
-  
-  
+  const [wordInd, setWordInd] = useState(Math.floor(Math.random() * words.length)); // starts with the first one 
+
+
   for (let i = 0; i < words[wordInd].length; i++) {
     styles.push("unTyped");
   }
@@ -98,16 +100,86 @@ function SecondStep() {
 
 
   const fullWord = charString.map((letter, ind) => {
-    if(pointer == words[wordInd].length) {
+    if (pointer == words[wordInd].length) {
       console.log("aa")
       updateWord();
       return (<h1 className={styles[ind]} key={ind}>{letter}</h1>)
     } else {
       return (<h1 className={styles[ind]} key={ind}>{letter}</h1>)
     }
-    
+
   });
 
+  let num = words[wordInd].charCodeAt(pointer)-97;
+  let leftPinky = 'circle'
+  let leftRingFinger = 'circle'
+  let leftMiddleFinger = 'circle'
+  let leftPointerFinger = 'circle'
+  let leftThumb = 'circle'
+  let rightPinky = 'circle'
+  let rightRingFinger = 'circle'
+  let rightMiddleFinger = 'circle'
+  let rightPointerFinger = 'circle'
+  let rightThumb = 'circle'
+  console.log(num);
+
+  if (num === 0) { //a
+    leftPinky = 'filledCircle'
+  } else if (num === 1) { // b
+    leftPointerFinger = 'filledCircle'
+    rightPointerFinger = 'filledCircle'
+  } else if (num === 2) { // c
+    leftPointerFinger = 'filledCircle'
+  } else if (num === 3) { // d
+    leftMiddleFinger = 'filledCircle'
+  } else if (num === 4) { //e 
+    leftMiddleFinger = 'filledCircle'
+  } else if (num === 5) { // f
+    leftPointerFinger = 'filledCircle'
+  } else if (num === 6) { //g
+    leftPointerFinger = 'filledCircle'
+  } else if (num === 7) { //h
+    rightPointerFinger = 'filledCircle'
+  } else if (num === 8) { //i
+    rightMiddleFinger = 'filledCircle'
+  } else if (num === 9) { //j
+    rightPointerFinger = 'filledCircle'
+  } else if (num === 10) { //k
+    rightMiddleFinger = 'filledCircle'
+  } else if (num === 11) { //l
+    rightRingFinger = 'filledCircle'
+  } else if (num === 12) { //m
+    rightPointerFinger = 'filledCircle'
+  } else if (num === 13) { //n
+    rightPointerFinger = 'filledCircle'
+  } else if (num === 14) { //o
+    rightRingFinger = 'filledCircle'
+  } else if (num === 15) { //p
+    rightRingFinger = 'filledCircle'
+  } else if (num === 16) { //q
+    leftRingFinger = 'filledCircle'
+  } else if (num === 17) { //r
+    leftPointerFinger = 'filledCircle'
+  } else if (num === 18) { //s
+    leftRingFinger = 'filledCircle'
+  } else if (num === 19) { //t
+    leftPointerFinger = 'filledCircle'
+  } else if (num === 20) { //u
+    rightPointerFinger = 'filledCircle'
+  } else if (num === 21) { //v
+    leftPointerFinger = 'filledCircle'
+  } else if (num === 22) { //w
+    leftRingFinger = 'filledCircle'
+  } else if (num === 23) { //x
+    leftRingFinger = 'filledCircle'
+  } else if (num === 24) { //y
+    rightPointerFinger = 'filledCircle'
+    leftPointerFinger = 'filledCircle'
+  } else if (num === 25) { //z
+    leftPinky = 'filledCircle'
+  } else if (num === 26) {
+    leftThumb = 'filledCircle'
+  }
 
 
   return (
@@ -118,9 +190,31 @@ function SecondStep() {
     //   animate={{ opacity: 1, x: 0 }}
     //   exit={{ opacity: 0, x: 100 }}
     // >
-    <div className='container'>
-      <KeyPressed />
-      {fullWord}
+    <div>
+      <div id="leftPinky" className={leftPinky}></div>
+      <div id="leftRingFinger" className={leftRingFinger}></div>
+      <div id="leftMiddleFinger" className={leftMiddleFinger}></div>
+      <div id="leftPointerFinger" className={leftPointerFinger}></div>
+      <div id="leftThumb" className={leftThumb}></div>
+
+      <div className='container'>
+        <div className='handsContainer'>
+          <img src={leftHand} className="lefthand" alt='Left Hand'></img>
+        </div>
+
+        <KeyPressed />
+        {fullWord}
+
+        <div className='handsContainer'>
+          <img src={rightHand} className="righthand" alt='Right Hand'></img>
+        </div>
+      </div>
+
+      <div id="rightPinky" className={rightPinky}></div>
+      <div id="rightRingFinger" className={rightRingFinger}></div>
+      <div id="rightMiddleFinger" className={rightMiddleFinger}></div>
+      <div id="rightPointerFinger" className={rightPointerFinger}></div>
+      <div id="rightThumb" className={rightThumb}></div>
     </div>
 
     // </motion.div>
